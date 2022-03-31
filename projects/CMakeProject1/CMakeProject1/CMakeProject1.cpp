4cb8f73e0fcf9a54e500a30f19ef0af0fb0663ac
zzj <912290893@qq.com>
2022-03-31 08:58:18 +0800

    asdf

---
+++ projects/CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,21 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+#include <boost/lexical_cast.hpp>
+#include <boost/timer/timer.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+using namespace std;
+using namespace boost;
+using namespace boost::gregorian;
+#pragma comment(lib, "libboost_date_time-vc143-mt-gd-x64-1_78.lib")
+int main()
+{
+	cout << "Hello CMake." << endl;
+    string s="12345";
+    //timer::cpu_timer at;
+    int a=lexical_cast<int>(s);
+    cout<<a<<endl;
+    date d1(2001, 1, 1);
+	return 0;
+}
---
+++ projects/CMakeProject1/cmake-build-debug/CMakeProject1/CMakeFiles/CMakeProject1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/zzj/Documents/workspace/cpp/demo02-cmake/CMakeProject1/CMakeProject1/CMakeProject1.cpp" "C:/Users/zzj/Documents/workspace/cpp/demo02-cmake/CMakeProject1/cmake-build-debug/CMakeProject1/CMakeFiles/CMakeProject1.dir/CMakeProject1.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/zzj/Documents/workspace/cpp/7z/install_dir/include/boost-1_78"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
